512a6ebc04c93c2e6283147aef7522b2bd75eac2
TarasGolub <taras.golub@gmail.com>
2016-08-10 15:36:16 +0300

    try use bootstrap

---
+++ public/blocks/header.php
@@ -0,0 +1,12 @@
+<div class="header">
+    <div class="header_logo">
+         <strong>Palo Alto</strong>
+    </div>
+
+    <ul class="menu_list">
+        <li class="menu_item"><a href="index.html" target="_self">Home</a></li>
+        <li class="menu_item"><a href="about.html" target="_self">About</a></li>
+        <li class="menu_item"><a href="archive.html" target="_self">Archive</a></li>
+        <li class="menu_item"><a href="contacts.html" target="_self">Contact</a></li>
+    </ul>
+</div>

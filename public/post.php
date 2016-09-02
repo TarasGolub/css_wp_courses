155742340e58f063382c3f45745b2b58393f8e55
TarasGolub <taras.golub@gmail.com>
2016-09-02 16:14:37 +0300

    done all effects specified in elements.psd file and correct other pages

--- public/post.php
+++ public/post.php
@@ -1,70 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php include("header.php");?>
-    <main class="main_page">
-        <article class="post article_post">
-            <header class="post_header">
-                <div class="post_date">
-                    Sep 11
+    <main class="main_page container">
+        <div class="row">
+
+            <article class="post article_post col">
+                <header class="post_header">
+                    <div class="post_date">
+                        Sep 11
+                    </div>
+                    <div class="i_do">
+                        <a href="#">I do reflect</a>
+                    </div>
+                    <h2>
+                        Democracy means simply the bludgeoning of the people by the people for the people.
+                    </h2>
+                </header>
+                <figure>
+                    <img src="images/img_6.jpg" alt="sculpture" title="sculpture">
+                </figure>
+                <div class="post_content top_line">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, cupiditate dicta distinctio explicabo
+                        id molestiae obcaecati placeat quisquam sint voluptatem? Ab adipisci aperiam at cupiditate, dignissimos
+                        doloremque dolorum ea, error eum facere fugiat illum labore laboriosam modi nam necessitatibus odio optio
+                        perferendis possimus quae qui quia repellat reprehenderit similique sit tempore vitae voluptate. Alias
+                        animi aut blanditiis distinctio dolorem in nam praesentium sed. A ducimus eaque necessitatibus officia
+                        recusandae! Eum exercitationem, voluptatem. A, aperiam consequatur cumque cupiditate dolor doloremque
+                        ducimus eaque eius error eum itaque iusto laboriosam mollitia nam odio
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, cupiditate dicta distinctio explicabo
+                        id molestiae obcaecati placeat quisquam sint voluptatem? Ab adipisci aperiam at cupiditate, dignissimos
+                        doloremque dolorum ea, error eum facere fugiat illum labore laboriosam modi nam necessitatibus odio optio
+                        perferendis possimus quae qui quia repellat reprehenderit similique sit tempore vitae voluptate. Alias
+                        animi aut blanditiis distinctio dolorem in nam praesentium sed. A ducimus eaque necessitatibus officia
+                        recusandae! Eum exercitationem, voluptatem. , error eum facere fugiat illum labore laboriosam modi nam
+                        necessitatibus odio optio possimus quae qui quia repellat reprehenderit similique sit tempore vitae
+                        voluptate. Alias
+                        animi aut blanditiis distinctio dolorem in nam praesentium sed. A ducimus eaque necessitatibus officia
+                        recusandae! Eum exercitationem, voluptatem.
+                        perferendis possimus quae qui quia  A, aperiam consequatur cumque cupiditate dolor doloremque
+                        ducimus eaque eius error eum itaque iusto laboriosam mollitia nam odio
+                        <a href="http://example.com/" target="_blank">placeat porro</a> provident quaerat
+                        recusandae sunt. Adipisci aliquam cum ex facilis maxime obcaecati officia repellendus tempora. Alias
+                        aliquam architecto corporis in.
+                    </p>
+                    <blockquote class="sculpture_quote">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus alias aperiam at atque cum
+                        cumque cupiditate ea, error eum explicabo id molestias perferendis quae, sequi sit tempore,
+                        veritatis voluptatem?
+                    </blockquote>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid consequatur dolorem
+                        eaque eveniet, explicabo facere incidunt maxime mollitia necessitatibus nostrum officiis porro sint
+                        totam ullam ut voluptas voluptate voluptatem!
+                    </p>
                 </div>
-                <div class="i_do">
-                    I do dreflect
-                </div>
-                <h2>
-                    Democracy means simply the bludgeoning of the people by the people for the people.
-                </h2>
-            </header>
-            <figure>
-                <img src="images/img_6.jpg" alt="sculpture" title="sculpture">
-            </figure>
-            <div class="post_content top_line">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, cupiditate dicta distinctio explicabo
-                    id molestiae obcaecati placeat quisquam sint voluptatem? Ab adipisci aperiam at cupiditate, dignissimos
-                    doloremque dolorum ea, error eum facere fugiat illum labore laboriosam modi nam necessitatibus odio optio
-                    perferendis possimus quae qui quia repellat reprehenderit similique sit tempore vitae voluptate. Alias
-                    animi aut blanditiis distinctio dolorem in nam praesentium sed. A ducimus eaque necessitatibus officia
-                    recusandae! Eum exercitationem, voluptatem. A, aperiam consequatur cumque cupiditate dolor doloremque
-                    ducimus eaque eius error eum itaque iusto laboriosam mollitia nam odio
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, cupiditate dicta distinctio explicabo
-                    id molestiae obcaecati placeat quisquam sint voluptatem? Ab adipisci aperiam at cupiditate, dignissimos
-                    doloremque dolorum ea, error eum facere fugiat illum labore laboriosam modi nam necessitatibus odio optio
-                    perferendis possimus quae qui quia repellat reprehenderit similique sit tempore vitae voluptate. Alias
-                    animi aut blanditiis distinctio dolorem in nam praesentium sed. A ducimus eaque necessitatibus officia
-                    recusandae! Eum exercitationem, voluptatem. , error eum facere fugiat illum labore laboriosam modi nam
-                    necessitatibus odio optio possimus quae qui quia repellat reprehenderit similique sit tempore vitae
-                    voluptate. Alias
-                    animi aut blanditiis distinctio dolorem in nam praesentium sed. A ducimus eaque necessitatibus officia
-                    recusandae! Eum exercitationem, voluptatem.
-                    perferendis possimus quae qui quia  A, aperiam consequatur cumque cupiditate dolor doloremque
-                    ducimus eaque eius error eum itaque iusto laboriosam mollitia nam odio
-                    <a href="http://example.com/" target="_blank">placeat porro</a> provident quaerat
-                    recusandae sunt. Adipisci aliquam cum ex facilis maxime obcaecati officia repellendus tempora. Alias
-                    aliquam architecto corporis in.
-                </p>
-                <blockquote class="sculpture_quote">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus alias aperiam at atque cum
-                    cumque cupiditate ea, error eum explicabo id molestias perferendis quae, sequi sit tempore,
-                    veritatis voluptatem?
-                </blockquote>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aliquid consequatur dolorem
-                    eaque eveniet, explicabo facere incidunt maxime mollitia necessitatibus nostrum officiis porro sint
-                    totam ullam ut voluptas voluptate voluptatem!
-                </p>
-            </div>
-            <footer class="post_footer">
-                <div class="read_more social_likes">
-                    Here will be social likes
-                </div>
-                <div class="hash_tag_container">
-                    <span class="hash_tag_item">Democracy</span>
-                    <span class="hash_tag_item">Introspection</span>
-                </div>
-            </footer>
-        </article>
+                <footer class="post_footer">
+                    <div class="read_more">
+                        <a href="#">read more</a>
+                    </div>
+                    <div class="hash_tag_container">
+                        <span class="hash_tag_item"><a href="#">#Democracy</a></span>
+                        <span class="hash_tag_item"><a href="#">#Introspection</a></span>
+                    </div>
+                </footer>
+            </article>
+
+        </div>
     </main>
     <?php include("footer.php"); ?>
 </html>
--- public/search.php
+++ public/search.php
@@ -1,179 +1,193 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php include("header.php");?>
-    <main class="search_page">
-    <!--i think next will be generate-->
-        <div class="post">
-            <header class="post_header search_header">
-                <h1>
-                    Search Results for &#171;interesting&#187;
-                </h1>
-                <p>
-                    found 10 items
-                </p>
-            </header>
-        </div>
-
-        <div class="search_result">
-            <article class="post text_post">
-                <header class="post_header">
-                    <div class="post_date">
-                        Sep 20
-                    </div>
-                    <div class="i_do">
-                        I do travel
-                    </div>
-                    <h2>
-                        No difference how many peaks you reach if there was no pleasure in the climb.
-                    </h2>
-                </header>
-                <div class="post_content top_line">
+    <main class="search_page container">
+        <div class="row">
+            <!--i think next will be generate-->
+            <div class="post">
+                <header class="post_header search_header">
+                    <h1>
+                        Search Results for &#171;interesting&#187;
+                    </h1>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur aut blanditiis debitis eos
-                        excepturi, nemo nisi nobis, officia, sint tenetur vel veritatis vitae? Adipisci consequatur dignissimos
-                        ex quia sed. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur aut blanditiis
-                        debitis eos excepturi, nemo nisi nobis, officia, sint tenetur vel veritatis vitae? Adipisci consequatur
-                        dignissimos ex quia sed.
+                        found 10 items
                     </p>
-                </div>
-                <footer class="post_footer">
-                    <div class="read_more">
-                        read more
-                    </div>
-                    <div class="hash_tag_container">
-                        <span class="hash_tag_item">Yosemite</span>
-                        <span class="hash_tag_item">Peak</span>
-                        <span class="hash_tag_item">Explorer</span>
-                    </div>
-                </footer>
-            </article>
-
-            <article class="post image_post">
-                <figure class="post_image">
-                    <img src="images/img_1.jpg" alt="image mountains " title="Mountains">
-                </figure>
-                <header class="post_header">
-                    <div class="post_date">
-                        Sep 18
-                    </div>
-                    <div class="i_do">
-                        I do observe
-                    </div>
-                    <h2>
-                        You know, I'd rather argue with you, then laugh with anyone else.
-                    </h2>
                 </header>
-                <div class="post_content top_line">
-                    <p class="coursive">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis cum explicabo officia placeat, unde
-                        velit vero vitae. Ad adipisci cumque, cupiditate dolorum enim esse officiis reiciendis tempora
-                        temporibus, totam veniam.
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequuntur dicta distinctio dolorum
-                        error exercitationem fugiat in, officiis quae vel. Aspernatur commodi fugit incidunt iste neque odit
-                        provident rerum tempora? amet, consectetur adipisicing elit. Assumenda consequuntur dicta distinctio
-                        dolorum error exercitationem fugiat in, officiis elit. Assumenda consequuntur dicta distinctio
-                        dolorum error exercitationem fugiat in, officiis
-                    </p>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequuntur dicta distinctio dolorum
-                        error exercitationem fugiat in, officiis quae vel. Aspernatur commodi fugit incidunt iste neque odit
-                        provident rerum tempora?
-                    </p>
-                </div>
-                <footer class="post_footer">
-                    <div class="read_more">
-                        read more
-                    </div>
-                    <div class="hash_tag_container">
-                        <span class="hash_tag_item">Yosemite</span>
-                        <span class="hash_tag_item">Peak</span>
-                        <span class="hash_tag_item">Photo</span>
-                    </div>
-                </footer>
-            </article>
+            </div>
 
-            <article class="post gallery_post">
-                <figure class="post_image">
-                    <img src="images/img_2.jpg" alt="gallery" title="gallery" class="no_padd">
-                </figure>
-                <header class="post_header">
-                    <div class="post_date">
-                        Sep 14
+            <div class="search_result">
+                <article class="post text_post col">
+                    <header class="post_header">
+                        <div class="post_date">
+                            Sep 20
+                        </div>
+                        <div class="i_do">
+                            <a href="#">I do travel</a>
+                        </div>
+                        <h2>
+                            No difference how many peaks you reach if there was no pleasure in the climb.
+                        </h2>
+                    </header>
+                    <div class="post_content top_line">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur aut blanditiis debitis eos
+                            excepturi, nemo nisi nobis, officia, sint tenetur vel veritatis vitae? Adipisci consequatur dignissimos
+                            ex quia sed. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur aut blanditiis
+                            debitis eos excepturi, nemo nisi nobis, officia, sint tenetur vel veritatis vitae? Adipisci consequatur
+                            dignissimos ex quia sed.
+                        </p>
                     </div>
-                    <div class="i_do">
-                        I do photo
-                    </div>
-                    <h2>
-                        Everything in the universe has a rhythm, everything dances.
-                    </h2>
-                </header>
-                <div class="post_content top_line">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae delectus enim expedita
-                        nesciunt. Eaque, fugit, ratione? Accusantium autem, eos facere harum illum ipsum neque nisi recusandae
-                        reiciendis rem? Nisi.
-                    </p>
-                </div>
-                <footer class="post_footer">
-                    <div class="read_more">
-                        read more
-                    </div>
-                    <div class="hash_tag_container">
-                        <span class="hash_tag_item">Yosemite</span>
-                        <span class="hash_tag_item">Photo</span>
-                        <span class="hash_tag_item">Hi-res</span>
-                    </div>
-                </footer>
-            </article>
+                    <footer class="post_footer">
+                        <div class="read_more">
+                            <a href="#">read more</a>
+                        </div>
+                        <div class="hash_tag_container">
+                            <span class="hash_tag_item"><a href="#">#Yosemite</a></span>
+                            <span class="hash_tag_item"><a href="#">#Peak</a></span>
+                            <span class="hash_tag_item"><a href="#">#Explorer</a></span>
+                        </div>
+                    </footer>
+                </article>
 
-            <article class="post video_post">
-                <figure class="post_image">
-                    <img src="images/img_3.jpg" alt="video" title="video" class="no_padd">
-                </figure>
-                <header class="post_header">
-                    <div class="post_date">
-                        Sep 14
+                <article class="post image_post col">
+                    <figure class="post_image single_image">
+                        <img src="images/img_1.jpg" alt="image mountains " title="Mountains">
+                        <div class="pin_it">
+                            <a href="http://pinterest.com"><img src="images/pin_it.png" alt="pin_it"></a>
+                        </div>
+                    </figure>
+                    <header class="post_header">
+                        <div class="post_date">
+                            Sep 18
+                        </div>
+                        <div class="i_do">
+                            <a href="#">I do observe</a>
+                        </div>
+                        <h2>
+                            You know, I'd rather argue with you, then laugh with anyone else.
+                        </h2>
+                    </header>
+                    <div class="post_content top_line">
+                        <p class="coursive">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis cum explicabo officia placeat, unde
+                            velit vero vitae. Ad adipisci cumque, cupiditate dolorum enim esse officiis reiciendis tempora
+                            temporibus, totam veniam.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequuntur dicta distinctio dolorum
+                            error exercitationem fugiat in, officiis quae vel. Aspernatur commodi fugit incidunt iste neque odit
+                            provident rerum tempora? amet, consectetur adipisicing elit. Assumenda consequuntur dicta distinctio
+                            dolorum error exercitationem fugiat in, officiis elit. Assumenda consequuntur dicta distinctio
+                            dolorum error exercitationem fugiat in, officiis
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequuntur dicta distinctio dolorum
+                            error exercitationem fugiat in, officiis quae vel. Aspernatur commodi fugit incidunt iste neque odit
+                            provident rerum tempora?
+                        </p>
                     </div>
-                    <div class="i_do">
-                        I do watch
-                    </div>
-                    <h2>
-                        As human beings, we have a natural compulsion to fill empty spaces.
-                    </h2>
-                </header>
-                <div class="post_content">
-                </div>
-                <footer class="post_footer">
-                    <div class="hash_tag_container video_hash_tag_container">
-                        <span class="hash_tag_item">Yosemite</span>
-                        <span class="hash_tag_item">Peak</span>
-                        <span class="hash_tag_item">Photo</span>
-                    </div>
-                </footer>
-            </article>
+                    <footer class="post_footer">
+                        <div class="read_more">
+                            <a href="#">read more</a>
+                        </div>
+                        <div class="hash_tag_container">
+                            <span class="hash_tag_item"><a href="#">#Yosemite</a></span>
+                            <span class="hash_tag_item"><a href="#">#Peak</a></span>
+                            <span class="hash_tag_item"><a href="#">#Photo</a></span>
+                        </div>
+                    </footer>
+                </article>
 
-            <article class="post quote_post">
-                <header class="post_header">
-                    <div class="i_do">
-                        I do quote
+                <article class="post gallery_post col">
+                    <figure class="post_image gallery_image">
+                        <img src="images/img_2.jpg" alt="gallery" title="gallery" class="no_padd">
+                        <div class="number_of_photo">21</div>
+                    </figure>
+                    <header class="post_header">
+                        <div class="post_date">
+                            Sep 14
+                        </div>
+                        <div class="i_do">
+                            <a href="#">I do photo</a>
+                        </div>
+                        <h2>
+                            Everything in the universe has a rhythm, everything dances.
+                        </h2>
+                    </header>
+                    <div class="post_content top_line">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae delectus enim expedita
+                            nesciunt. Eaque, fugit, ratione? Accusantium autem, eos facere harum illum ipsum neque nisi recusandae
+                            reiciendis rem? Nisi.
+                        </p>
                     </div>
-                </header>
-                <div class="post_content">
-                    <blockquote class="quote">
-                        There is always something left to love.
-                        And if you ain’t learned that, you
-                        ain’t learned nothing.
-                    </blockquote>
+                    <footer class="post_footer">
+                        <div class="read_more">
+                            <a href="#">read more</a>
+                        </div>
+                        <div class="hash_tag_container">
+                            <span class="hash_tag_item"><a href="#">#Yosemite</a></span>
+                            <span class="hash_tag_item"><a href="#">#Photo</a></span>
+                            <span class="hash_tag_item"><a href="#">#Hi-res</a></span>
+                        </div>
+                    </footer>
+                </article>
+
+                <article class="post video_post col">
+                    <figure class="post_image">
+                        <img src="images/img_3.jpg" alt="video" title="video" class="no_padd">
+                    </figure>
+                    <header class="post_header">
+                        <div class="post_date">
+                            Sep 14
+                        </div>
+                        <div class="i_do">
+                            <a href="#">I do watch</a>
+                        </div>
+                        <h2>
+                            As human beings, we have a natural compulsion to fill empty spaces.
+                        </h2>
+                    </header>
+                    <div class="post_content">
+                    </div>
+                    <footer class="post_footer">
+                        <div class="hash_tag_container video_hash_tag_container">
+                            <span class="hash_tag_item"><a href="#">#Yosemite</a></span>
+                            <span class="hash_tag_item"><a href="#">#Peak</a></span>
+                            <span class="hash_tag_item"><a href="#">#Photo</a></span>
+                        </div>
+                    </footer>
+                </article>
+
+                <article class="post quote_post col">
+                    <header class="post_header">
+                        <div class="i_do">
+                            <a href="#">I do quote</a>
+                        </div>
+                    </header>
+                    <div class="post_content">
+                        <blockquote class="quote">
+                            There is always something left to love.
+                            And if you ain’t learned that, you
+                            ain’t learned nothing.
+                        </blockquote>
+                    </div>
+                    <footer class="post_footer">
+                        <div class="hash_tag_container quote_author">
+                            <span>— Lorraine Hansberry</span>
+                        </div>
+                    </footer>
+                </article>
+
+                <div class="page_pointer_container">
+                    <div class="page_pointer page_pointer_current">1</div>
+                    <div class="page_pointer">2</div>
+                    <div class="page_pointer">3</div>
                 </div>
-                <footer class="post_footer">
-                    <div class="hash_tag_container quote_author">
-                        <span>— Lorraine Hansberry</span>
-                    </div>
-                </footer>
-            </article>
+            </div>
         </div>
     </main>
     <?php include("footer.php"); ?>
 </html>
+
+

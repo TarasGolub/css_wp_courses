64d3985a77653743731a5ad8f8787726ab5a4088
TarasGolub <taras.golub@gmail.com>
2016-09-03 08:41:22 +0300

    done drop-out menu for mobile

--- public/footer.php
+++ public/footer.php
@@ -1,77 +1,79 @@
-<footer class="page_footer">
-    <div class="container">
-        <div class="logo footer_logo">
-            <a href="index.php" >Palo Alto</a>
-        </div>
-        <div class="menu footer_menu row">
-            <nav>
-                <div class="menu footer_menu_item col m-offset-2 m-2">
-                    <a href="index.php" >Home</a>
-                </div>
-                <div class="menu footer_menu_item col m-2">
-                    <a href="about.php" >About</a>
-                </div>
-                <div class="menu footer_menu_item col m-2">
-                    <a href="archive.php" >Archive</a>
-                </div>
-                <div class="menu footer_menu_item col m-2">
-                    <a href="contacts.php" >Contact</a>
-                </div>
-                <div class="search_icon_footer col m-1">
-                    <a href="search.php"><span class="search_icon">&#128269;</span></a>
-                </div>
-            </nav>
-        </div>
-        <div class="row">
-            <div class="footer_info top_line col ">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda cupiditate deleniti
-                    facere facilis inventore maiores natus numquam tempora unde veniam.
-                    dolor sit amet, consectetur adipisicing elit. Assumenda cupiditate deleniti
-                    facere facilis inventore maiores natus
-                </p>
+    <footer class="page_footer">
+        <div class="container">
+            <div class="logo footer_logo">
+                <a href="index.php" >Palo Alto</a>
             </div>
-        </div>
+            <div class="menu footer_menu row">
+                <nav>
+                    <div class="menu footer_menu_item col m-offset-2 m-2">
+                        <a href="index.php" >Home</a>
+                    </div>
+                    <div class="menu footer_menu_item col m-2">
+                        <a href="about.php" >About</a>
+                    </div>
+                    <div class="menu footer_menu_item col m-2">
+                        <a href="archive.php" >Archive</a>
+                    </div>
+                    <div class="menu footer_menu_item col m-2">
+                        <a href="contacts.php" >Contact</a>
+                    </div>
+                    <div class="search_icon_footer col m-1">
+                        <a href="search.php"><span class="search_icon">&#128269;</span></a>
+                    </div>
+                </nav>
+            </div>
+            <div class="row">
+                <div class="footer_info top_line col ">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda cupiditate deleniti
+                        facere facilis inventore maiores natus numquam tempora unde veniam.
+                        dolor sit amet, consectetur adipisicing elit. Assumenda cupiditate deleniti
+                        facere facilis inventore maiores natus
+                    </p>
+                </div>
+            </div>
 
-        <div class="row">
-            <div class="rights_reserved top_line col ">
-                <p>
-                    © 2015 - Palo Alto. All Rights Reserved. Designed & Developed
-                    by <a href="http://example.com/" target="_blank">Pixel Buddha Team</a>
-                </p>
+            <div class="row">
+                <div class="rights_reserved top_line col ">
+                    <p>
+                        © 2015 - Palo Alto. All Rights Reserved. Designed & Developed
+                        by <a href="http://example.com/" target="_blank">Pixel Buddha Team</a>
+                    </p>
+                </div>
             </div>
-        </div>
 
-        <div class="social_container">
-            <div class="social">
-                <div class="social_item">
-                    <a href="http://facebook.com" target="_blank">
-                        <img src="images/facebook.png" alt="facebook" title="facebook">
-                    </a>
-                </div>
-                <div class="social_item">
-                    <a href="http://twitter.com" target="_blank">
-                        <img src="images/twitter.png" alt="twitter" title="twitter">
-                    </a>
-                </div>
-                <div class="social_item">
-                    <a href="http://instagram.com" target="_blank">
-                        <img src="images/instagram.png" alt="instagram" title="instagram">
-                    </a>
-                </div>
-                <div class="social_item">
-                    <a href="http://pinterest.com" target="_blank">
-                        <img src="images/pinterest.png" alt="pinterest" title="pinterest">
-                    </a>
+            <div class="social_container">
+                <div class="social">
+                    <div class="social_item">
+                        <a href="http://facebook.com" target="_blank">
+                            <img src="images/facebook.png" alt="facebook" title="facebook">
+                        </a>
+                    </div>
+                    <div class="social_item">
+                        <a href="http://twitter.com" target="_blank">
+                            <img src="images/twitter.png" alt="twitter" title="twitter">
+                        </a>
+                    </div>
+                    <div class="social_item">
+                        <a href="http://instagram.com" target="_blank">
+                            <img src="images/instagram.png" alt="instagram" title="instagram">
+                        </a>
+                    </div>
+                    <div class="social_item">
+                        <a href="http://pinterest.com" target="_blank">
+                            <img src="images/pinterest.png" alt="pinterest" title="pinterest">
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</footer>
-<script src="js/jquery.maphilight.min.js"></script>
-<script src="js/nprogress.js"></script>
-<script type="text/javascript">
-    NProgress.start(); // начать "загрузку"
-    NProgress.done(); // заставить индикатор дойти до конца и пропасть
-</script>
+    </footer>
+    <script src="js/jquery.maphilight.min.js"></script>
+    <script src="js/nprogress.js"></script>
+    <script type="text/javascript">
+        NProgress.start(); // начать "загрузку"
+        NProgress.done(); // заставить индикатор дойти до конца и пропасть
+    </script>
+<!--//end of all_page_wrapper div-->
+</div>
 </body>
